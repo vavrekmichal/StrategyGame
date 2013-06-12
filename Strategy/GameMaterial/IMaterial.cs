e0b1477bef44496f6a6421eea7026c291f80f884
Michal Vavrek <vavrekmichal@gmail.com>
2013-06-12 18:21:19 +0200

    material commented

--- Strategy/GameMaterial/IMaterial.cs
+++ Strategy/GameMaterial/IMaterial.cs
@@ -6,12 +6,31 @@ using Strategy.GameObjectControl.Game_Objects.StaticGameObjectBox;
 using Strategy.GameObjectControl.RuntimeProperty;
 
 namespace Strategy.GameMaterial {
+	/// <summary>
+	/// Represents game material and controls creating of the game objects (a required quantity).
+	/// </summary>
     public interface IMaterial {
+		/// <summary>
+		/// Returns reference on Property with a current quantity of the material. Object with reference on this Property
+		/// can see the current quantity of the material.
+		/// </summary>
+		/// <returns>Returns refence to material's Property with a current quantity.</returns>
         Property<int> GetQuantityOfMaterial();
-        bool TryBuild(int wantedQuantity);
+
+		/// <summary>
+		/// Adds given quantity of the material.
+		/// </summary>
+		/// <param name="quantity">The adding quantities of material.</param>
         void AddQuantity(double quantity);
 
+		/// <summary>
+		/// Returns current quantity of material.
+		/// </summary>
         int State {  get; }
+
+		/// <summary>
+		/// Returns a name of the material.
+		/// </summary>
         string Name { get; }
     }
 }
--- Strategy/GameMaterial/Matrial.cs
+++ Strategy/GameMaterial/Matrial.cs
@@ -5,38 +5,55 @@ using System.Text;
 using Strategy.GameObjectControl.RuntimeProperty;
 
 namespace Strategy.GameMaterial {
+	/// <summary>
+	/// Represents an universal game material (name is setted by constructor).
+	/// </summary>
     class Matrial :IMaterial{
         private string myName;
-        private Property<int> actualQuantity;
+        private Property<int> currentQuantity;
 		double remain;
 
+		/// <summary>
+		/// Creates instance and sets name of the material.
+		/// </summary>
+		/// <param name="name"></param>
         public Matrial(string name) {
             myName = name;
-			actualQuantity = new Property<int>(0);
+			currentQuantity = new Property<int>(0);
         }
 
+		/// <summary>
+		/// Returns material name.
+		/// </summary>
         public string Name {
             get { return myName; }
         }
 
+		/// <summary>
+		/// Returns reference to currentQuantity Property. 
+		/// </summary>
+		/// <returns>Returns reference to Property with a current quantity.</returns>
 		public Property<int> GetQuantityOfMaterial() {
-			return actualQuantity;
+			return currentQuantity;
         }
 
-        public bool TryBuild(int wantedQuantity) {
-            throw new NotImplementedException();
-        }
-
+		/// <summary>
+		/// Returns current quantity of the material. 
+		/// </summary>
         public int State {
-            get { return (int)actualQuantity.Value; }
+            get { return (int)currentQuantity.Value; }
         }
 
+		/// <summary>
+		/// Adds given quantity of the material. To currentQuantity adds just integers. Fractional part stores and adds to it a next time. 
+		/// </summary>
+		/// <param name="quantity">The adding quantities of material.</param>
         public void AddQuantity(double quantity) {
 			remain += quantity;
 			if (remain>1) {
 				int increment = (int)Math.Truncate(remain);
 				remain -= increment;
-				actualQuantity.Value += increment;
+				currentQuantity.Value += increment;
 			}
           
         }
