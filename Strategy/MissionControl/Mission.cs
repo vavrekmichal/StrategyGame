dc94d7ad7c73dd840ce9115b6fe6729412fbeae0
Michal Vavrek <vavrekmichal@gmail.com>
2013-06-14 13:56:30 +0200

    mission commented

--- Strategy/MissionControl/Mission.cs
+++ Strategy/MissionControl/Mission.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Strategy.GameObjectControl.Game_Objects.GameTargets;
 using Strategy.GameObjectControl.RuntimeProperty;
 
 namespace Strategy.MissionControl {
+	/// <summary>
+	/// Represents a game mission. Can be loaded just one (before loading next mission the firts
+	/// must be destroyd).
+	/// </summary>
 	public class Mission {
 
 		List<ITarget> targetList;
@@ -14,10 +15,17 @@ namespace Strategy.MissionControl {
 		const string winText = "You are winner.";
 		const string targetCompEffect = "TargetComp.wav";
 
+		/// <summary>
+		/// Initializes Mission (no mission targets).
+		/// </summary>
 		public Mission() {
 			targetList = new List<ITarget>();
 		}
 
+		/// <summary>
+		/// Initializes mission targets. If the initialization of the target
+		/// fails so the target is removed as complete.
+		/// </summary>
 		public void Initialize() {
 			foreach (var target in new List<ITarget>(targetList)) {
 				if (!target.Initialize()) {
@@ -26,8 +34,14 @@ namespace Strategy.MissionControl {
 			}
 		}
 
+		/// <summary>
+		/// Updates all mission targets and checks if any exists (if not, so the
+		/// mission end.)
+		/// </summary>
+		/// <param name="delay">The delay between last two frames.</param>
 		public void Update(float delay) {
 			if (targetList.Count() == 0) {
+				// No target -> mission ends.
 				Game.EndGame(winText);
 			}
 			foreach (var target in new List<ITarget>(targetList)) {
@@ -39,10 +53,18 @@ namespace Strategy.MissionControl {
 			}
 		}
 
+		/// <summary>
+		/// Inserts non-initializes mission target.
+		/// </summary>
+		/// <param name="target">The inserting mission target.</param>
 		public void AddTarget(ITarget target) {
 			targetList.Add(target);
 		}
 
+		/// <summary>
+		/// Collects informaion about all mission targets to the List.
+		/// </summary>
+		/// <returns>Returns the List with infomations about mission targets.</returns>
 		public List<Property<string>> GetMissionInfo() {
 			var list = new List<Property<string>>();
 			foreach (var target in targetList) {
